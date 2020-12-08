1ad390b00ccf234164c239064fad5a4c20673e61
Radu Nicolescu <34610198+raduncc@users.noreply.github.com>
2020-12-08 20:10:17 +0200

    added CHAD script

--- check_results.sh
+++ check_results.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
+echo '===BUILDING==='
 make
+
+echo '===GETTING IMAGES==='
+
 ./sequential tests/darth-vader.pgm darth_seq
 ./sequential tests/macro.pnm macro_seq
-for i in 1 2 4
-do
-	./pthreads tests/darth-vader.pgm darth_p{i} i
-	./omp_imp tests/darth-vader.pgm darth_o{i} i
-	./omp_imp2 tests/darth-vader.pgm darth_o2{i} i
-	./pthreads tests/macro.pnm macro_p{i} i
-	./omp_imp tests/macro.pnm macro_o{i} i
-	./omp_imp tests/macro.pnm macro_o2{i} i
-done
 
 for i in 1 2 4
 do
-	./compare darth_seq darth_p{i}
-	./compare darth_seq darth_o{i}
-	./compare darth_seq darth_o2{i}
-
-	./compare macro_seq macro_p{i}
-	./compare macro_seq macro_o{i}
-	./compare macro_seq macro_o2{i}
+	./pthreads tests/darth-vader.pgm darth_p$i $i
+	./openmp tests/darth-vader.pgm darth_o$i $i
+	./openmp2 tests/darth-vader.pgm darth_o2$i $i
+	./pthreads tests/macro.pnm macro_p$i $i
+	./openmp tests/macro.pnm macro_o$i $i
+	./openmp2 tests/macro.pnm macro_o2$i $i
 done
 
+echo '===COMPARING RESULTS==='
+for i in 1 2 4
+do
+	echo 'Comparing darth_seq with dart parallel with $i threads'
+	./compare darth_seq darth_p$i
+	echo 'Comparing darth_seq with dart OMP1 with $i threads'
+	./compare darth_seq darth_o$i
+	echo 'Comparing darth_seq with dart OMP2 with $i threads'
+	./compare darth_seq darth_o2$i
+
+
+	echo 'Comparing macro_seq with macro parallel with $i threads'
+	./compare macro_seq macro_p$i
+	echo 'Comparing macro_seq with macro OMP1 with $i threads'
+	./compare macro_seq macro_o$i
+	echo 'Comparing macro_seq with macro OMP2 with $i threads'
+	./compare macro_seq macro_o2$i
+done
+
+echo '===CLEANING==='
 
 make clean
 rm macro*
