477f50fb5e9f95a95fb20b631f2c9dc8e7d15a9c
Radu Nicolescu <34610198+raduncc@users.noreply.github.com>
2020-12-08 19:36:24 +0200

    added openmp implem, deleted test files to fit 1.5

--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@ build:
 	gcc compare.c -o compare
 	gcc sequential_imp.c -o sequential
 	gcc pthreads_imp.c -lm -lpthread -o pthreads
+	gcc omp_imp.c -lm -fopenmp -o openmp
 
 clean:
 	rm compare
 	rm sequential
 	rm pthreads
+	rm openmp
---
+++ omp_imp.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <omp.h>
+#include <math.h>
+
+int N;
+int P;
+int width, height;
+int maxval;
+int size;
+unsigned char *img;
+unsigned char *img_new;
+
+void read_image(char *name)
+{
+    FILE *f = fopen(name, "r");
+    char *aux = malloc(2);
+    if (!aux)
+        return;
+    fscanf(f, "%s\n", aux);
+    P = aux[1] - 48;
+    // printf("%d\n", P);
+    aux = realloc(aux, 500);
+    fgets(aux, 500, f);
+    fscanf(f, "%d %d\n", &width, &height);
+    // printf("%d %d\n", width, height);
+    fscanf(f, "%d\n", &maxval);
+    // printf("%d\n", maxval);
+    free(aux);
+    if (P == 5)
+    {
+        size = width * height;
+    }
+    else if (P == 6)
+    {
+        size = width * height * 3;
+    }
+    // printf("%d\n", size);
+    img = malloc(size);
+    if (!img)
+    {
+        return;
+    }
+    for (int i = 0; i < size; ++i)
+    {
+        fscanf(f, "%c", &img[i]);
+    }
+}
+
+void edit_image(int id)
+{
+    int start, stop, i, j, offset_start = 0, offset_end = 0;
+    unsigned char min_val;
+
+    float aux = ((float)width) / N;
+    start = ceil(aux) * id;
+    stop = fmin(ceil(aux) * (id + 1), width);
+    if (P == 5)
+    {
+        if (N == 1)
+        {
+            offset_start = 1;
+            offset_end = -1;
+        }
+        else
+        {
+            if (id == 0)
+            {
+                offset_start = 1;
+            }
+            if (id == N - 1)
+            {
+                offset_end = -1;
+            }
+        }
+    }
+    else if (P == 6)
+    {
+        if (N == 1)
+        {
+            offset_start = 3;
+            offset_end = -1;
+        }
+        else
+        {
+            if (id == 0)
+            {
+                offset_start = 3;
+            }
+            if (id == N - 1)
+            {
+                offset_end = -1;
+            }
+        }
+    }
+
+    if (P == 5)
+    {
+        for (i = 1; i < height - 1; ++i)
+        {
+            for (j = start + offset_start; j < stop + offset_end; ++j)
+            {
+                min_val = 255;
+                for (int a = -1; a <= 1; ++a)
+                {
+                    for (int b = -1; b <= 1; ++b)
+                    {
+                        if (img[(i + a) * width + j + b] < min_val)
+                            min_val = img[(i + a) * width + j + b];
+                    }
+                }
+                img_new[i * width + j] = min_val;
+            }
+        }
+    }
+
+    else if (P == 6)
+    {
+        for (i = 1; i < height - 1; ++i)
+        {
+            for (j = 3 * start + offset_start; j < 3 * stop + offset_end; ++j)
+            {
+                min_val = 255;
+                //red channel
+                for (int a = -1; a <= 1; ++a)
+                {
+                    for (int b = -3; b <= 3; b += 3)
+                    {
+                        if (img[(i + a) * 3 * width + j + b] < min_val)
+                            min_val = img[(i + a) * 3 * width + j + b];
+                    }
+                }
+                img_new[i * 3 * width + j] = min_val;
+                //green channel
+                j++;
+                min_val = 255;
+                for (int a = -1; a <= 1; ++a)
+                {
+                    for (int b = -3; b <= 3; b += 3)
+                    {
+                        if (img[(i + a) * 3 * width + j + b] < min_val)
+                            min_val = img[(i + a) * 3 * width + j + b];
+                    }
+                }
+                img_new[i * 3 * width + j] = min_val;
+                //blue channel
+                j++;
+                min_val = 255;
+                for (int a = -1; a <= 1; ++a)
+                {
+                    for (int b = -3; b <= 3; b += 3)
+                    {
+                        if (img[(i + a) * 3 * width + j + b] < min_val)
+                            min_val = img[(i + a) * 3 * width + j + b];
+                    }
+                }
+                img_new[i * 3 * width + j] = min_val;
+            }
+        }
+    }
+}
+
+void write_image(char *name)
+{
+    FILE *f = fopen(name, "w");
+    fprintf(f, "P%d\n", P);
+    fprintf(f, "%d %d\n", width, height);
+    fprintf(f, "%d\n", maxval);
+    for (int i = 0; i < size; ++i)
+    {
+        fprintf(f, "%c", img_new[i]);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 4) {
+        printf("Not enough arguments! Usage: ./openmp file_in_name file_out_name num_threads\n");
+        return -1;
+    }
+    char *name_in = strdup(argv[1]);
+    char *name_out = strdup(argv[2]);
+    N = atoi(argv[3]);
+    read_image(name_in);
+    img_new = malloc(size);
+    if (!img_new)
+    {
+        return -1;
+    }
+    omp_set_num_threads(N);
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        edit_image(ID);
+    // edit_image();
+    }
+    write_image(name_out);
+    return 0;
+}
